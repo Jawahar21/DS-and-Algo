16b67049fc9d213fb032e7132e6867cbe71a8443
Sai Jawahar Prudhivi <p.sai.jawahar@gmail.com>
2020-10-04 16:44:44 +0530

    Find anagrams problem

--- DS and Algo/problems/HashTable.cs
+++ DS and Algo/problems/HashTable.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -369,6 +368,54 @@ namespace DS_and_Algo.problems
                 }
             }
             return 0;
+        }
+
+        /*
+         * Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+
+            Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+            The order of output does not matter.
+
+            Example 1:
+            Input:
+            s: "cbaebabacd" p: "abc"
+            
+            Output:
+            [0, 6]
+            
+            Explanation:
+            The substring with start index = 0 is "cba", which is an anagram of "abc".
+            The substring with start index = 6 is "bac", which is an anagram of "abc".
+            
+            Example 2:
+            Input:
+            s: "abab" p: "ab"
+            
+            Output:
+            [0, 1, 2]
+            
+            Explanation:
+            The substring with start index = 0 is "ab", which is an anagram of "ab".
+            The substring with start index = 1 is "ba", which is an anagram of "ab".
+            The substring with start index = 2 is "ab", which is an anagram of "ab".
+         * **/
+        public static IList<int> FindAnagrams(string s, string p)
+        {
+            IList<int> result = new List<int>();
+            int binaryHashOfP = 0;
+            foreach (char c in p) {
+                binaryHashOfP |= 1 << (c - 'a');
+            }
+            for (int i = 0; i < s.Length - p.Length + 1; i++) {
+                int binaryHashOfSubS = 0;
+                for (int j = i; j < i + p.Length; j++) {
+                    binaryHashOfSubS |= 1 << (s[j] - 'a');
+                }
+                if (binaryHashOfSubS == binaryHashOfP) {
+                    result.Add(i);
+                }
+            }
+            return result;
         }
     }
 }
