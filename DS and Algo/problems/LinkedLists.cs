68171be6938f94133cbd9ff7a619d3efe2924177
Sai Jawahar Prudhivi <p.sai.jawahar@gmail.com>
2020-10-07 22:54:22 +0530

    Delete duplicates

--- DS and Algo/problems/LinkedLists.cs
+++ DS and Algo/problems/LinkedLists.cs
@@ -21,14 +21,16 @@ namespace DS_and_Algo.problems
         {
             ListNode first = new ListNode(0);
             ListNode curr = first;
-            while (l1 != null && l2 != null) {
-                
+            while (l1 != null && l2 != null)
+            {
+
                 if (l1.val < l2.val)
                 {
                     curr.next = l1;
                     l1 = l1.next;
                 }
-                else {
+                else
+                {
                     curr.next = l2;
                     l2 = l2.next;
                 }
@@ -37,6 +39,45 @@ namespace DS_and_Algo.problems
             if (l1 == null) curr.next = l2;
             if (l2 == null) curr.next = l1;
             return first.next;
+        }
+
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null) return head;
+            ListNode curr = head;
+            while (curr.next != null)
+            {
+                if (curr.val == curr.next.val)
+                {
+                    curr.next = curr.next.next;
+                }
+                else
+                {
+                    curr = curr.next;
+                }
+            }
+            return head;
+        }
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int sumNum = GetNumFromList(l1) + GetNumFromList(l2);
+            if (sumNum == 0) return new ListNode(0);
+            return GetListFromNum(sumNum);
+        }
+
+        private static int GetNumFromList(ListNode l)
+        {
+            if (l == null) return 0;
+            return GetNumFromList(l.next) * 10 + l.val;
+        }
+
+        private static ListNode GetListFromNum(int num)
+        {
+            if (num == 0) return null;
+            ListNode currNode = new ListNode(num % 10);
+            currNode.next = GetListFromNum(num / 10);
+            return currNode;
         }
     }
 }
