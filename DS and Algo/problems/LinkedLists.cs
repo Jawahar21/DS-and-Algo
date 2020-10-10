046aa608bb01a806e9cffaa77d96a74e4519262c
Sai Jawahar Prudhivi <p.sai.jawahar@gmail.com>
2020-10-10 22:49:00 +0530

    Swap Pairs problem

--- DS and Algo/problems/LinkedLists.cs
+++ DS and Algo/problems/LinkedLists.cs
@@ -152,5 +152,21 @@ namespace DS_and_Algo.problems
             }
             return index;
         }
+
+        /*
+         * Given a linked list, swap every two adjacent nodes and return its head.
+         * You may not modify the values in the list's nodes. Only nodes itself may be changed.
+         * 
+         * Input: head = [1,2,3,4]
+         * Output: [2,1,4,3]
+         * **/
+        public static ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+            ListNode nextNode = head.next;
+            head.next = SwapPairs(head.next.next);
+            nextNode.next = head;
+            return nextNode;
+        }
     }
 }
