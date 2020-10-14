343cddff46279844e8a11759ba784f6e73867ed5
Sai Jawahar Prudhivi <p.sai.jawahar@gmail.com>
2020-10-14 23:39:39 +0530

    Detect cycle

--- DS and Algo/problems/LinkedLists.cs
+++ DS and Algo/problems/LinkedLists.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace DS_and_Algo.problems
@@ -261,6 +262,35 @@ namespace DS_and_Algo.problems
                 }
             }
             return distinctList.next;
+        }
+
+        /*
+         * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+            There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+            Notice that you should not modify the linked list.
+            Follow up:
+            Can you solve it using O(1) (i.e. constant) memory?
+         */
+        public static ListNode DetectCycle(ListNode head)
+        {
+            if (head == null || head.next == null) return null;
+            ListNode start = head, slow = head, fast = head;
+            while(fast!= null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if(slow == fast)
+                {
+                    while(slow!=start)
+                    {
+                        start = start.next;
+                        slow = slow.next;
+                    }
+                    return start;
+                }
+            }
+            return null;
         }
     }
 }
