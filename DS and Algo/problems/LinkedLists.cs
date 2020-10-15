9069172e8ccf06d83981fca44d86329b9b4504e5
Sai Jawahar Prudhivi <p.sai.jawahar@gmail.com>
2020-10-15 23:10:02 +0530

    Partition problem

--- DS and Algo/problems/LinkedLists.cs
+++ DS and Algo/problems/LinkedLists.cs
@@ -292,5 +292,36 @@ namespace DS_and_Algo.problems
             }
             return null;
         }
+
+        /*
+         * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+         * You should preserve the original relative order of the nodes in each of the two partitions.
+         * Example:
+         * Input: head = 1->4->3->2->5->2, x = 3
+         * Output: 1->2->2->4->3->5
+         * **/
+        public static ListNode Partition(ListNode head, int x)
+        {
+            ListNode listLessThanX = new ListNode(0);
+            ListNode resultList = listLessThanX;
+            ListNode listGreaterOrEqualToX = new ListNode(0);
+            ListNode greaterListHead = listGreaterOrEqualToX;
+            while (head!= null)
+            {
+                if(head.val < x)
+                {
+                    listLessThanX.next = head;
+                    listLessThanX = listLessThanX.next;
+                }
+                else
+                {
+                    listGreaterOrEqualToX.next = head;
+                    listGreaterOrEqualToX = listGreaterOrEqualToX.next;
+                }
+                head = head.next;
+            }
+            listLessThanX.next = greaterListHead.next;
+            return resultList.next;
+        }
     }
 }
