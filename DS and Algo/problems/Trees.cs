0a963dd3beb7abbf1666929907051a88b3301c6d
Sai Jawahar Prudhivi <p.sai.jawahar@gmail.com>
2020-11-10 23:10:12 +0530

    IsSymmetric problem

--- DS and Algo/problems/Trees.cs
+++ DS and Algo/problems/Trees.cs
@@ -15,6 +15,13 @@
 
     public class Trees
     {
+        /// <summary>
+        /// Given two binary trees, write a function to check if they are the same or not.
+        /// Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
         public static bool IsSameTree(TreeNode p, TreeNode q)
         {
             if (p == null && q == null) return true;
@@ -23,6 +30,31 @@
                 return (IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
             }
             return false;
+        }
+
+        /// <summary>
+        /// Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+        /// For example, this binary tree[1, 2, 2, 3, 4, 4, 3] is symmetric:
+        ///     1
+        ///    / \
+        ///   2   2
+        ///  / \ / \
+        /// 3  4 4  3
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+
+            return IsMirror(root.left, root.right);
+        }
+
+        private bool IsMirror(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null) return true;
+            if (left?.val != right?.val) return false;
+            return IsMirror(left.left, right.right) && IsMirror(left.right, right.left);
         }
     }
 }
